8cb2ec72faf4341e8dca14cc519ee944a8bac417
BuildTools <unconfigured@null.spigotmc.org>
2023-03-07 13:25:41 +0900

    forgot to update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="SoUtils",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     description="SoUtils - Utility Library",
     author="sou"
